376f5cbf34db4685da541c6b15cfe28394ae1ad6
ipunganwar <ipung.anwar1@gmail.com>
2024-05-25 23:22:04 +0700

    feat: add bcrypt

--- actions/register.ts
+++ actions/register.ts
@@ -1,6 +1,9 @@
 "use server";
 
 import * as z from "zod";
+import bcrypt from "bcrypt";
+
+import { db } from "@/lib/db";
 import { RegisterSchema } from "@/schemas";
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
@@ -10,5 +13,28 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: "invalid fields!" };
   }
 
-  return { success: "Email sent!" };
+  const { email, password, name } = validatedFields.data;
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const exsistingUser = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (exsistingUser) {
+    return { error: "Email already in use!" };
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // TODO: send verification token email
+
+  return { success: "User created!" };
 };
