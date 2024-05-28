a06b5c58d553585f6b9a6da00589bfdb3f46d0fd
ipunganwar <ipung.anwar1@gmail.com>
2024-05-29 01:19:20 +0700

    feat: auth config

--- auth.config.ts
+++ auth.config.ts
@@ -1,4 +1,29 @@
-import GitHub from "next-auth/providers/github";
+import bcrypt from "bcrypt";
 import type { NextAuthConfig } from "next-auth";
+import Credential from "next-auth/providers/credentials";
 
-export default { providers: [GitHub] } satisfies NextAuthConfig;
+import { LoginSchema } from "@/schemas";
+import { getUserByEmail } from "@/data/user";
+
+export default {
+  providers: [
+    Credential({
+      async authorize(credentials) {
+        const validatedFields = LoginSchema.safeParse(credentials);
+
+        if (validatedFields.success) {
+          const { email, password } = validatedFields.data;
+
+          const user = await getUserByEmail(email);
+          if (!user || !user.password) return null;
+
+          const passwordMatch = await bcrypt.compare(password, user.password);
+
+          if (passwordMatch) return user;
+        }
+
+        return null;
+      },
+    }),
+  ],
+} satisfies NextAuthConfig;
