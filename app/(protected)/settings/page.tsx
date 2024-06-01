3d4ffa8c03c4c25b1696bade9a621a8f07479ac6
ipunganwar <ipung.anwar1@gmail.com>
2024-06-01 13:20:41 +0700

    feat: signout

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,10 +1,23 @@
-import { auth } from "@/auth";
+import { auth, signOut } from "@/auth";
 
 type Props = {};
 const SettingPage = async ({}: Props) => {
   const session = await auth();
 
-  return <div>{JSON.stringify(session)}</div>;
+  return (
+    <div>
+      {JSON.stringify(session)}
+
+      <form
+        action={async () => {
+          "use server";
+          await signOut();
+        }}
+      >
+        <button type="submit">Sign Out</button>
+      </form>
+    </div>
+  );
 };
 
 export default SettingPage;
