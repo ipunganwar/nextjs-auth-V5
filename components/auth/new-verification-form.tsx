28a078bbba1271346115ff4987ebe5448e9ea829
ipunganwar <ipung.anwar1@gmail.com>
2024-06-16 12:32:14 +0700

    feat: apply actions into new-verification-form

--- components/auth/new-verification-form.tsx
+++ components/auth/new-verification-form.tsx
@@ -1,10 +1,40 @@
 "use client";
 
+import { useCallback, useEffect, useState } from "react";
 import { BeatLoader } from "react-spinners";
 import { CardWrapper } from "@/components/auth/card-wrapper";
+import { useSearchParams } from "next/navigation";
+import { newVerification } from "@/actions/new-verification";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
 
 type Props = {};
 export const NewVerificationForm = ({}: Props) => {
+  const [error, setError] = useState<string | undefined>();
+  const [success, setSuccess] = useState<string | undefined>();
+
+  const searchParams = useSearchParams();
+  const token = searchParams.get("token");
+
+  const onSubmit = useCallback(() => {
+    if (!token) {
+      setError("Missing token!");
+      return;
+    }
+    newVerification(token)
+      .then((data) => {
+        setSuccess(data?.success);
+        setError(data?.error);
+      })
+      .catch(() => {
+        setError("Something went wrong!");
+      });
+  }, [token]);
+
+  useEffect(() => {
+    onSubmit();
+  }, [onSubmit]);
+
   return (
     <CardWrapper
       headerLabel="Confirming your verification"
@@ -12,7 +42,10 @@ export const NewVerificationForm = ({}: Props) => {
       backButtonLabel="Back to login"
     >
       <div className="flex items-center w-full justify-center">
-        <BeatLoader />
+        {!success && !error && <BeatLoader />}
+
+        <FormSuccess message={success} />
+        <FormError message={error} />
       </div>
     </CardWrapper>
   );
