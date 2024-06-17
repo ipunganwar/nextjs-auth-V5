33af454c46b3bce4a8bc126f85e0c688fc5064aa
ipunganwar <ipung.anwar1@gmail.com>
2024-06-17 09:52:41 +0700

    feat: create email 2FA

--- lib/mail.ts
+++ lib/mail.ts
@@ -2,6 +2,15 @@ import { Resend } from "resend";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
+export const sendTwoFactorEmail = async (email: string, token: string) => {
+  const { data, error } = await resend.emails.send({
+    from: "Acme <onboarding@resend.dev>",
+    to: email,
+    subject: "2FA Code",
+    html: `<p>Your 2FA Code: ${token}</p>`,
+  });
+};
+
 export const sendVerificationEmail = async (email: string, token: string) => {
   const confirmLink = `http://localhost:3000/auth/new-verification?token=${token}`;
 
